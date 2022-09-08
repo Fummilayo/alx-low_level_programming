6e40b440c4426fd4e64c368723e03d7105291e01
Esther <Oluwatimilehinayomide93@gmail.com>
2022-09-08 07:15:03 -0700

    6-size.c commit2

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,8 +4,13 @@
  *
  * Return: Always 0 (success)
  */
-int main(void)
-{printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+int main(void){
+int a;
+long int b;
+long long int c;
+char d;
+float f;
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
