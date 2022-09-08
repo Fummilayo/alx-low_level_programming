7162cf131383e94cce3b43c46eb5073e307c62dc
Esther <Oluwatimilehinayomide93@gmail.com>
2022-09-08 08:02:46 -0700

    .6-size.c.swp commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ int main(void)
      float f;
 
 printf("Size of a char: %ld byte(s)\n", sizeof(c));
-printf("Size of a int: %ld byte(s)\n", sizeof(i));
+printf("Size of an int: %ld byte(s)\n", sizeof(i));
 printf("Size of a long int: %ld byte(s)\n", sizeof(ii));
 printf("Size of a long long int: %ld byte(s)\n", sizeof(iii));
 printf("Size of a float: %ld byte(s)\n", sizeof(f));
