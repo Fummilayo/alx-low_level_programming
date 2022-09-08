c3fec02069abb1607323ab3428c4f1cd264a65b9
Esther <Oluwatimilehinayomide93@gmail.com>
2022-09-08 04:54:44 -0700

    4-puts.c First commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,9 @@
-#!/bin/bash
-puts("Programming is like building a multilingual puzzle\n");return(0)
+      #include <stdio.h>
+
+   int main(void)
+{
+
+puts("Programming is like building a multilingual puzzle"\n");
+return(0);
+
+}	
