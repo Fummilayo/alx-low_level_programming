6660f4156e9cfe1d88f91d51b3b04acc63841e74
Esther <Oluwatimilehinayomide93@gmail.com>
2022-09-08 06:01:01 -0700

    5-printf.c commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main is a function with no argument.
+*printf is a function used to print standard output.
+*return o: checks if the operstion is a success
+*
+*/
+main(void)
+{printf("with proper grammar, but the outcome is a piece of art");
+return (0);
+}	 
