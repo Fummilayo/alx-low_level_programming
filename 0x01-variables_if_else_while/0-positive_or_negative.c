27c644389fc6e74a0f88a16bdaeb9ca1669669c0
Esther <Oluwatimilehinayomide93@gmail.com>
2022-09-09 04:03:31 -0700

    0-positive_or_negative.c commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+/**
+*Main - Entry point
+*
+*Return: 0(success)
+*
+*/
+
+int main(void)
+{int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if( n > 0)
+{
+printf("n is positive \n");
+}
+if( n == 0)
+{
+printf("n is zero \n");
+}	
+if( n < 0)
+{
+printf("n is negative \n");
+}	
+return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+This repository answers the variables, if else and while tasks
