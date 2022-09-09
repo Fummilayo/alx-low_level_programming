da8113b15ae5aa84d00d892a065d1176a189bfc2
Esther <Oluwatimilehinayomide93@gmail.com>
2022-09-09 07:46:41 -0700

    8-print_base16.c  commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{int hexa_decimal;
+char letter;
+for( hexa_decimal = 0; hexa_decimal < 10; hexadecimal++)
+{
+putchar((hexa_decimal % 10) + '0');
+}
+for (letter = 'a'; letter <= 'f'; letter++)
+{
+putchar(letter);
+}
+putchar('\n');
+return (0);
+}
