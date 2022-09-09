e1d62f59c885a137d1b5ae4b537bb48fa5fff94b
Esther <Oluwatimilehinayomide93@gmail.com>
2022-09-09 06:12:18 -0700

    3-print_alphabets.c  COMMIT123

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,12 +8,10 @@ int main(void)
 {
 char lowercase;
 char uppercase;
-for (lowercase = 'a'; lowercase <= 'z'; lowercase++){
+for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
 putchar(lowercase);
-}
-for (uppercase = 'A'; uppercase <= 'Z'; uppercase++){
+for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)
 putchar(uppercase);
-}
 putchar('\n');
 return (0);
 }
