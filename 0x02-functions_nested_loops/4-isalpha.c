a9ab0bbedf68465be396e51021d90b58914b1e9f
Esther <Oluwatimilehinayomide93@gmail.com>
2022-09-13 07:00:51 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,5 +5,5 @@
  *Return: 1 if int c is lowercase, 0 if otherwise
  */
 int _isalpha(int c)
-{ return ( c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z');
+{ return (( c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
