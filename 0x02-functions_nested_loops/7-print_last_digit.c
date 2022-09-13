086b4a7203d08eca6850503abbb516db60decef9
Esther <Oluwatimilehinayomide93@gmail.com>
2022-09-13 08:39:14 -0700

    7-print_last_digit.c commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,8 +6,11 @@
  *
  */
 int print_last_digit(int n)
-{ int x = n % 10;
-
+{ int last_digit = n % 10;
+if (last_digit < 0)
+last_digit *= -1;
+_putchar(last_digit + '0');
+return (last_digit);
 
 
 
