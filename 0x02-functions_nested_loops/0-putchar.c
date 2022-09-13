519efc85faaf47c336aafb8fef1f88a95eb888f8
Esther <Oluwatimilehinayomide93@gmail.com>
2022-09-13 04:27:05 -0700

    0-putchar.c  commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *main - Entry point.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{ char *sh = "Holberton";
+while (*sh)
+{
+_putchar(*sh);
+sh++;
+}
+_putchar('\n');
+return (0);
+}
