63b508ffc4bdfe707b61881472b4c2aee7b041c7
Esther <Oluwatimilehinayomide93@gmail.com>
2022-09-13 06:24:19 -0700

    2-print_alphabet_x10.c commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,19 +4,16 @@
  *
  */
 void print_alphabet_x10(void)
-{ int i = 0;
-while (i < 10)
+{ int i;
+int alphabet;
+for (i = 0; i < 10; i++)
 {
-int alphabet = 'a';
-while (alphabet < 'z')
+ for(alphabet = 'a'; alphabet < 'z'; alphabet++)
 {
 _putchar(alphabet);
-alphabet++;
 }
-_putchar('\n');
-i++;
+putchar('\n');
 }
 }
-
 
 
