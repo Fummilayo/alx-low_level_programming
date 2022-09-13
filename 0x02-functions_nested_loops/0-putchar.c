b3e19a45be59e6717c2d3039126e07169558a737
Esther <Oluwatimilehinayomide93@gmail.com>
2022-09-13 04:44:13 -0700

    0-putchar.c  commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,25 @@
-#include <main.h>
+#include <unistd.h>
+#include "main.h"
+
+
 /**
- *main - Entry point.
  *
- * Return: Always 0 (success)
- */
-int main(void)
-{ char *sh = "Holberton";
-while (*sh)
+ *  * _putchar - writes the character c to stdout
+ *
+ *   * @c: The character to print
+ *
+ *    *
+ *
+ *     * Return: On success 1.
+ *
+ *      * On error, -1 is returned, and errno is set appropriately.
+ *
+ *       */
+
+int _putchar(char c)
+
 {
-_putchar(*sh);
-sh++;
-}
-_putchar('\n');
-return (0);
+
+		return (write(1, &c, 1));
+
 }
