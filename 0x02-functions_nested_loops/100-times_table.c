be2bdb77c61146db1f7c9d20e4afdac641573d95
Esther <Oluwatimilehinayomide93@gmail.com>
2022-09-13 20:24:00 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,18 +10,18 @@ void print_times_table(int n)
 { int rol, con, p;
 if (n <= 15 && n > 0)
 {
-for (rol = 0; rol <= n; ++rol)
+for (rol = 0; rol <= n; rol++)
 {
 _putchar('0');
 _putchar(',');
-for (con = 1; con <= n; ++con)
+for (con = 1; con <= n; con++)
 {
 p = con * rol;
 _putchar(' ');
 if (p / 100  >= 1)
 {
 _putchar('0' + p / 100);
-_putchar('0' + p / 10 );
+_putchar('0' + p / 10 % 10);
 _putchar('0' + p % 10);
 }
 else if (p / 10 >= 1)
