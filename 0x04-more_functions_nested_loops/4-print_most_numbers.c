7d5f4f6948e1ae8800d377d94fe3df296ad39333
Esther <Oluwatimilehinayomide93@gmail.com>
2022-09-15 05:16:34 -0700

    4-print_most_numbers.c  commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers -  a function that prints the numbers, from 0 to 9
+ * Return: Always 0
+ *
+ */
+void print_most_numbers(void)
+{ int c;
+for (c = 48; c <= 57; c++)
+{
+if (c == 50 && c == 52)
+{
+continue;
+_putchar(c);
+}
+}
+_putchar('\n');
+}
