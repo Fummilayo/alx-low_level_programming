d37bc1dbb7b482a1cf098c20e67cf235b5e1f970
Esther <Oluwatimilehinayomide93@gmail.com>
2022-09-19 09:22:41 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+char *_strcpy(char *dest, char *src)
+{ int length;
+length = strcpy(dest, src);
+dest = '\0';
+return (dest);
+}
