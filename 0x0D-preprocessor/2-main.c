661d6ca3a203bdfae1b1794ab85f07ace8cbc125
Esther <oluwatimilehinayomide93@gmail.com>
2022-10-10 05:51:51 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 /**
  * main - Entry point
  * Return: it returns 0
  */
 int main(void)
 {
-printf("%s\n", _FILE_);
+printf("%s\n",  __FILE__);
 return (0);
 }
