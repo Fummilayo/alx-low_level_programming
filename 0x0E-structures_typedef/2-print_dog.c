aa9c96abc7d70db753a513249d96799222446eb9
Esther <oluwatimilehinayomide93@gmail.com>
2022-10-10 18:59:59 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,12 +15,7 @@ printf("Name: %s\n", d->name);
 }
 else
 printf("Name: (nil)\n");
-if (d->age != NULL)
-{
 printf("Age: %.6f\n", d->age);
-}
-else
-printf("Age: (nil)\n");
 if (d->owner != NULL)
 printf("Owner: %s\n", d->owner);
 else
